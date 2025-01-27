e75468b88ee962134324271bc9c66245b211a015
Juan Manuel Paredes <139470808+juanparedes26@users.noreply.github.com>
2025-01-27 23:51:21 +0000

    termine

---
+++ app.js
@@ -0,0 +1,25 @@
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension=[".com",".net",".us",".io"];
+
+
+for(ele of pronoun){
+    
+    for(ele2 of adj){
+        
+        for(ele3 of noun){
+
+            for(ele4 of extension){
+
+                console.log(ele+ele2+ele3+ele4);
+
+
+        }
+
+
+    }
+
+
+}
+}
